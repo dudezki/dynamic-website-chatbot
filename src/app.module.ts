a78ba9f16c987f3b0629c12d3b01cd35db9c0819
Lucky John Faderon <luckyf@callboxinc.com>
2025-06-12 08:10:42 +0800

    Updated: restore to previous

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { GeminiModule } from './gemini/gemini.module';
-import { DatabaseModule } from './database/database.module';
-import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -12,11 +8,7 @@ import { AuthModule } from './auth/auth.module';
       isGlobal: true,
       envFilePath: '.env',
     }),
-    DatabaseModule,
-    AuthModule,
     GeminiModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
